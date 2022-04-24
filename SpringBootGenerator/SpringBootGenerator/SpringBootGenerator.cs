ce878f1e0c6ec688c07d38ebd2432423977c1298
Felix <trutschf@edu.htl-villach.at>
2022-04-24 15:02:22 +0200

    Structure V2
    * DTOs
    * Abstracted Ids via ObjectId
    * Abstracted Services via EntityService and GetSingleService
    * Dockerization
    * run script
    * fix tests
    * configure application.properties

--- SpringBootGenerator/SpringBootGenerator/SpringBootGenerator.cs
+++ SpringBootGenerator/SpringBootGenerator/SpringBootGenerator.cs
@@ -1,99 +1,244 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SpringBootGenerator {
 	public static class Generator {
-		public static int Entity(string entityName, string packagePath, string packageName) {
-			JavaTemplater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/Class.template").WithName(entityName).InPackage(packageName).In("models").WriteTo(packagePath);
-			JavaTemplater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/Repository.template").WithName(entityName).InPackage(packageName).In("repositories").WriteTo(packagePath, "Repository");
-			JavaTemplater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/Service.template").WithName(entityName).InPackage(packageName).In("services").WriteTo(packagePath, "Service");
-			JavaTemplater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/Controller.template").WithName(entityName).InPackage(packageName).In("controllers").WriteTo(packagePath, "Controller");
+		private static string projectRoot = ".";
+		private static string projectName;
+		private static string port = "8080";
+		private static string appImageName;
+		private static string appName = "tru_app";
+		private static string dbName = "truDb";
+		private static string dbUser = "usr";
+		private static string dbPassword = "pwd";
+
+
+		public static string ProjectRoot {
+			get { return projectRoot; }
+			set {
+				if (value != null) projectRoot = value;
+			}
+		}
+		public static string ProjectName {
+			get { return projectName; }
+			set {
+				if (value != null) projectName = value;
+			}
+		}
+
+		public static string Port {
+			get { return port; }
+			set {
+				if (value != null) port = value;
+			}
+		}
+		public static string AppImageName {
+			get {
+				if (appImageName == null) {
+					return appName;
+				} else {
+					return appImageName;
+				}
+			}
+			set {
+				if (value != null) appImageName = value;
+			}
+		}
+		public static string AppName {
+			get { return appName; }
+			set {
+				if (value != null) appName = value;
+			}
+		}
+		public static string DbName {
+			get { return dbName; }
+			set {
+				if (value != null) dbName = value;
+			}
+		}
+		public static string DbUser {
+			get { return dbUser; }
+			set {
+				if (value != null) dbUser = value;
+			}
+		}
+		public static string DbPassword {
+			get { return dbPassword; }
+			set {
+				if (value != null) dbPassword = value;
+			}
+		}
+
+		public static int Setup(string packageName) {
+			string templateDir = AppDomain.CurrentDomain.BaseDirectory + "/Templates";
+			string packageDirs = packageName.Replace('.', '/');
+			string sourceDir = ProjectRoot + "/src/main/java/" + packageDirs;
+
+			Templater.FromFile(templateDir + "/ObjectId.template")
+				.Set("package", packageName)
+				.ToFile(sourceDir + "/models/ObjectId.java")
+				;
+
+			Templater.FromFile(templateDir + "/EntityService.template")
+				.Set("package", packageName)
+				.ToFile(sourceDir + "/services/EntityService.java")
+				;
+
+			Templater.FromFile(templateDir + "/GetSingleService.template")
+				.Set("package", packageName)
+				.ToFile(sourceDir + "/services/GetSingleService.java")
+				;
+
+			Templater.FromFile(templateDir + "/test.Util.template")
+				.Set("package", packageName)
+				.ToFile(ProjectRoot + "/src/test/java/" + packageDirs + "/controllers/Util.java");
+
+			return 0;
+		}
+
+		public static int Entity(string entityName, string packageName) {
+			string templateDir = AppDomain.CurrentDomain.BaseDirectory + "/Templates";
+			string packageDirs = packageName.Replace('.', '/');
+			string sourceDir = ProjectRoot + "/src/main/java/" + packageDirs;
+
+			Setup(packageName);
+
+			Templater.FromFile(templateDir + "/Entity.template")
+				.Set("entity", entityName)
+				.Set("package", packageName)
+				.ToFile(sourceDir + "/models/" + entityName + "/" + entityName + ".java")
+				;
+			Templater.FromFile(templateDir + "/CreateEntity.template")
+				.Set("entity", entityName)
+				.Set("package", packageName)
+				.ToFile(sourceDir + "/models/" + entityName + "/Create" + entityName + ".java")
+				;
+
+			Templater.FromFile(templateDir + "/Repository.template")
+				.Set("entity", entityName)
+				.Set("package", packageName)
+				.ToFile(sourceDir + "/repositories/" + entityName + "Repository.java")
+				;
+
+			Templater.FromFile(templateDir + "/Service.template")
+				.Set("entity", entityName)
+				.Set("package", packageName)
+				.ToFile(sourceDir + "/services/" + entityName + "Service.java")
+				;
+
+			Templater.FromFile(templateDir + "/Controller.template")
+				.Set("lowerEntity", entityName.ToLower())
+				.Set("package", packageName)
+				.Set("entity", entityName)
+				.ToFile(sourceDir + "/controllers/" + entityName + "Controller.java");
+
+			Templater.FromFile(templateDir + "/test.Controller.template")
+				.Set("package", packageName)
+				.Set("entity", entityName)
+				.Set("lowerEntity", entityName.ToLower())
+				.ToFile(ProjectRoot + "/src/test/java/" + packageDirs + "/controllers/" + entityName + "ControllerTest.java");
 
 			return 4;
 		}
 
-		//public static void Dockerization(path) {
-		//	DockerfileTemplater.FromFile("Templates/dockerfile.template");
-		//}
+		public static int FixDbForTests() {
+			Templater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/application.properties.test.template")
+				.Set("dbUser", dbUser)
+				.Set("dbPassword", dbPassword)
+				.Set("port", port)
+				.ToFile(ProjectRoot + "/src/test/resources/application.properties");
 
-		//private class DockerfileTemplater {
-		//	private readonly string templateString;
+			return 1;
+		}
 
-		//	public DockerfileTemplater(string templateString) {
-		//		this.templateString = templateString;
-		//	}
+		public static void Config() {
+			Templater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/application.properties.template")
+				.Set("port", port)
+				.Set("dbUser", dbUser)
+				.Set("dbPassword", dbPassword)
+				.ToFile(ProjectRoot + "/src/main/resources/application.properties");
+		}
 
-		//	public static DockerfileTemplater FromFile(string filePath) {
-		//		return new DockerfileTemplater(File.ReadAllText(filePath));
-		//	}
+		public static void Runscript() {
+			Templater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/build.cmd.template")
+				.Set("appImageName", AppImageName)
+				.Set("appName", AppName)
+				.Set("dbName", DbName)
+				.ToFile(ProjectRoot + "/build.cmd");
+		}
 
-		//	public string Applied() {
-		//		templateString.Replace("~", );
-		//	}
-		//}
+		public static void Dockerization() {
+			Templater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/DockerFile.template")
+				.Set("port", Port)
+				.ToFile(ProjectRoot + "/dockerfile");
 
-		private class JavaTemplater {
-			private readonly string templateString;
+			Templater.FromFile(AppDomain.CurrentDomain.BaseDirectory + "/Templates/DockerCompose.template")
+				.Set("port", Port)
+				.Set("appName", AppName)
+				.Set("appImageName", AppImageName)
+				.Set("dbName", DbName)
+				.Set("dbUser", DbUser)
+				.Set("dbPassword", DbPassword)
+				.ToFile(ProjectRoot + "/docker-compose.yml");
+		}
 
-			private string entityName;
-			private string packageIdentifier;
-			private string finalPackage;
+		internal class Templater {
+			public string TemplateString { get; protected set; }
 
-			public JavaTemplater(string templateString) {
-				this.templateString = templateString;
+			public Templater(string templateString) {
+				this.TemplateString = templateString;
 			}
 
-			public static JavaTemplater FromFile(string filePath) {
-				return new JavaTemplater(File.ReadAllText(filePath));
+			public static Templater FromFile(string filePath) {
+				return new Templater(File.ReadAllText(filePath));
 			}
 
-			public JavaTemplater WithName(string entityName) {
-				this.entityName = entityName;
+			public string Replace(string a, string b) {
+				return TemplateString.Replace(a, b);
+			}
+
+			public Templater Set(string tag, string value) {
+				TemplateString = Replace("[{" + tag + "}]", value);
 				return this;
 			}
 
-			/// <param name="packageIdentifier">
-			///		Example: <code>at.company.project</code>
-			/// </param>
-			public JavaTemplater InPackage(string packageIdentifier) {
-				this.packageIdentifier = packageIdentifier;
-				return this;
+			private static string OnlyDirectory(string path) {
+				var split = path.Split('/');
+				var splitRemoved = new List<string>();
+				for (int i = 0; i < split.Length - 1; i++) {
+					splitRemoved.Add(split[i]);
+				}
+
+				var joined = "";
+				foreach (string val in splitRemoved) {
+					joined += "/" + val;
+				}
+				joined = joined.Remove(0, 1);
+
+				return joined;
 			}
 
-			public JavaTemplater In(string finalPackage) {
-				this.finalPackage = finalPackage;
-				return this;
-			}
-
-			private string Applied() {
-				string tmp = "package " + packageIdentifier + "." + finalPackage + ";\n";
-				tmp += templateString.Replace("~", entityName).Replace("[{package}]", packageIdentifier);
-
-				return tmp;
-			}
-
-			public void WriteTo(string pathToProjectRoot, string fileNameAddon="") {
-				var fileName = entityName + fileNameAddon + ".java";
-
-				var path = pathToProjectRoot + "/src/main/java/" + packageIdentifier.Replace('.', '/') + "/" + finalPackage;
-				Directory.CreateDirectory(path);
+			public virtual void ToFile(string filePath) {
+				var dirs = OnlyDirectory(filePath);
+				Directory.CreateDirectory(dirs);
 				try {
-					File.WriteAllText(path + "/" + fileName, this.Applied());
-					Console.WriteLine(finalPackage.ToUpper() + "...CREATED");
-					Console.WriteLine("  Filename: " + fileName);
+					File.WriteAllText(filePath, TemplateString);
+					Console.WriteLine(filePath.ToUpper() + "...CREATED");
+					Console.WriteLine("  Filename: " + filePath);
 				} catch (PathTooLongException ptle) {
-					Console.WriteLine(finalPackage.ToUpper() + "...ERROR");
+					Console.WriteLine(filePath.ToUpper() + "...ERROR");
 					Console.WriteLine("  Error: PATH TOO LONG");
 					Console.WriteLine(ptle.Message);
 				} catch (IOException ioe) {
-					Console.WriteLine(finalPackage.ToUpper() + "...ERROR");
+					Console.WriteLine(filePath.ToUpper() + "...ERROR");
 					Console.WriteLine("  Error: IO-ERROR");
 					Console.WriteLine(ioe.Message);
 				}
 			}
 		}
-
 	}
 
 }
--- SpringBootGenerator/SpringBootGeneratorCli/Program.cs
+++ SpringBootGenerator/SpringBootGeneratorCli/Program.cs
@@ -1,38 +1,95 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using SpringBootGenerator;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SpringBootGeneratorCli {
 	class Program {
+		static string SetValue(string value) {
+			if (value.StartsWith("--")) return null;
+			return value;
+		}
+
 		static int Main(string[] args) {
-			string entityName = null;
-			if (args.Length > 0) entityName = args[0];
-
-			string packageIdentifier = null;
-			if (args.Length > 1) packageIdentifier = args[1];
-
-			string path = ".";
-			if (args.Length > 2) path = args[2];
-
-			if (entityName == null || packageIdentifier == null) {
-				Console.WriteLine("EntityName or packageIdentifier not given!");
-				Console.WriteLine("Usage: sbg <EntityName> <packageIdentifier> [<path='.'>]");
+			bool arg(string arg) {
+				foreach (string argument in args) {
+					if (argument.ToLower() == arg.ToLower()) return true;
+				}
+				return false;
+			}
+			int argIndex(string arg) {
+				for (int i = 0; i < args.Length; i++) {
+					if (args[i].ToLower() == arg.ToLower()) return i;
+				}
 				return -1;
 			}
-
-			bool dockerization = false;
-			bool createRunscript = false;
-			foreach (string arg in args) {
-				if (arg == "--docker" || arg == "--dockerization") dockerization = true;
-				if (arg == "--script") createRunscript = true;
+			string val(string argName) {
+				foreach (string argument in args) {
+					if (argument.ToLower().StartsWith(argName.ToLower()+"=")) {
+						return argument.Replace(argName + "=", "");
+					}
+				}
+				return null;
+			}
+			string SetValue(int index) {
+				if (args.Length <= index) return null;
+				return Program.SetValue(args[index]);
 			}
 
-			return SpringBootGenerator.Generator.Entity(entityName, path, packageIdentifier);
+			if (args.Length == 0 || arg("--help")) {
+				showHelp();
+				return 0;
+			}
 
-			//if (dockerization) {
-			//	SpringBootGenerator.Generator.Dockerization(path);
-			//}
-			//if (createRunscript) {
-			//	SpringBootGenerator.Generator.Runscript(dockerization, path);
-			//}
+			Generator.ProjectRoot = val("--root");
+			Generator.ProjectName = val("--name");
+			Generator.Port = val("--port");
+			Generator.AppImageName = val("--imageName");
+			Generator.AppName = val("--appName");
+			Generator.DbName = val("--dbName");
+			Generator.DbUser = val("--dbUser");
+			Generator.DbPassword = val("--dbPassword");
+			Generator.ProjectName = val("--name");
+
+			if (arg("--entity")) {
+				int index = argIndex("--entity");
+
+				string entityName = SetValue(++index);
+				string packageIdentifier = SetValue(++index);
+
+				if (entityName == null || packageIdentifier == null) {
+					Console.WriteLine("EntityName or packageIdentifier not given!");
+					showHelp();
+					return -1;
+				}
+
+				Generator.Entity(entityName, packageIdentifier);
+			}
+
+			if (arg("--fix-tests")) Generator.FixDbForTests();
+			if (arg("--config")) Generator.Config();
+			if (arg("--docker") || arg("--dockerization")) Generator.Dockerization();
+			if (arg("--script")) Generator.Runscript();
+
+			return 0;
+		}
+
+		static void showHelp() {
+			Console.WriteLine("SpringBootGenerator");
+			Console.WriteLine("Usage: sbg [--entity <EntityName> <packageIdentifier>] [--fix-tests] [--docker] [--script] [--config]\n" +
+							  "       [--root='.'] [--name=...] [--port=8080] [--imageName=app21] [--appName=tru_app] [--dbName=truDb]\n" +
+							  "       [--dbUser=usr] [--dbPassword=pwd]");
+			Console.WriteLine();
+			Console.WriteLine("--entity......Create an Entity with given Name in the given package.\n" +
+							 "               The packageIdentifier has to point to the package in which\n" +
+							 "               the model, repository, controller and so on is.");
+			Console.WriteLine("--fix-tests...Creates a correct application.properties file for the tests\n" +
+							 "               uses the 'h2' db, so make sure to have that installed.");
+			Console.WriteLine("--docker......Creates a dockerfile and a dockercompose file that contains\n" +
+							 "               also the db.");
+			Console.WriteLine("--script......Creates a script to build, package and run the whole project.");
+			Console.WriteLine("--config......Creates the application.properties file for normal usage.");
 		}
 	}
 }
